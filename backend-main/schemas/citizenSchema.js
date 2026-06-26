c8b2bd757c92ab35a20496a4ae0ed2f3ba7c483c
sujal <sujalpatil3867@gmail.com>
2026-06-26 15:45:32 +0530

    updated the sql schema and zod schema for the police and citizens

--- backend-main/schemas/citizenSchema.js
+++ backend-main/schemas/citizenSchema.js
@@ -1,17 +1,17 @@
 import { z } from "zod";
 
 const loginCitizenSchema = z.object({
-  aadhar_card: z.string().length(12, "Aadhar card must be exactly 12 digits").regex(/^\d+$/, "Aadhar card must contain only digits"),
-  password: z.string().min(10, "password must be 10 character long")
+  aadhar_card: z.string().trim().length(12, "Aadhar card must be exactly 12 digits").regex(/^\d+$/, "Aadhar card must contain only digits"),
+  password: z.string().trim().min(1, "password is required")
 }).strict()
 
 
 const registerCitizenSchema = z.object({
-  aadhar_card: z.string().length(12, "Aadhar card must be exactly 12 digits").regex(/^\d+$/, "Aadhar card must contain only digits"),
-  password: z.string().min(10, "password must be 10 character long"),
+  aadhar_card: z.string().trim().length(12, "Aadhar card must be exactly 12 digits").regex(/^\d+$/, "Aadhar card must contain only digits"),
+  password: z.string().trim().min(1, "password is required"),
   photo: z.string().trim().optional(),
-  email: z.email().optional(),
-  phone_no: z.number(),
+  email: z.string().email().optional(),
+  phone_no: z.string().regex(/^[6-9]\d{9}$/, "Invalid Indian phone number").optional(),
   full_name: z.string().trim().min(1, "full name is required"),
   address: z.string().trim().min(1, "address is required"),
   city: z.string().trim().min(1, "city is required"),
